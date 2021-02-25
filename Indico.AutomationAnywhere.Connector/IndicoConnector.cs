186e31d8318ca976eb43fa7811279e4af196c409
Szymon Socha <szymon.socha@venturedevs.com>
2021-02-25 13:20:37 +0100

    INT-49 Added ListSubmissions method to the connector.

--- Indico.AutomationAnywhere.Connector/IndicoConnector.cs
+++ Indico.AutomationAnywhere.Connector/IndicoConnector.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IndicoV2;
 using IndicoV2.Submissions;
+using IndicoV2.Submissions.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Indico.AutomationAnywhere.Connector
 {
@@ -53,5 +57,52 @@ namespace Indico.AutomationAnywhere.Connector
 
             return Task.Run(async () => await _submissionsClient.CreateAsync(workflowId, uris.Select(u => new Uri(u)))).GetAwaiter().GetResult().ToArray();
         }
+
+        public string ListSubmissions(int[] submissionIds, int[] workflowIds, string inputFileName, string status, string retrieved, int limit = 1000)
+        {
+            SubmissionStatus? parsedStatus = null;
+            bool? parsedRetrieved = null;
+
+            if (string.IsNullOrWhiteSpace(inputFileName))
+            {
+                inputFileName = null;
+            }
+
+            if (!string.IsNullOrEmpty(status))
+            {
+                if (!Enum.TryParse(status, out SubmissionStatus statusValue))
+                {
+                    throw new ArgumentException("Wrong status value provided. Please provide one of the valid submission statuses.");
+                }
+                else
+                {
+                    parsedStatus = statusValue;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(retrieved))
+            {
+                if (!bool.TryParse(retrieved, out bool retrievedValue))
+                {
+                    throw new ArgumentException("Wrong retreived value provided. Please provide \"True\" or \"False\" as a value.");
+                }
+                else
+                {
+                    parsedRetrieved = retrievedValue;
+                }
+            }
+
+            var submissionFilter = new SubmissionFilter
+            {
+                InputFilename = inputFileName,
+                Status = parsedStatus,
+                Retrieved = Convert.ToBoolean(retrieved)
+            };
+
+            var submissions = Task.Run(async () => await _submissionsClient.ListAsync(submissionIds, workflowIds, submissionFilter, limit)).GetAwaiter().GetResult();
+
+            return JsonConvert.SerializeObject(submissions, new StringEnumConverter());
+        }
+
     }
 }
