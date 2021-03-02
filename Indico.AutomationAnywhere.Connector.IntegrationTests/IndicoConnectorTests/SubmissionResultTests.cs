e82c51aa01e139bd2d63c2378f201abeaa45e3aa
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-02 11:39:09 +0100

    INT-50 Added integration test for Submission Result action.

---
+++ Indico.AutomationAnywhere.Connector.IntegrationTests/IndicoConnectorTests/SubmissionResultTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Indico.AutomationAnywhere.Connector.IntegrationTests.IndicoConnectorTests
+{
+    public class SubmissionResultTests : IntegrationTestsBase
+    {
+        [Test]
+        public async Task SubmissionResult_ShouldReturnJobResult()
+        {
+            // Arrange
+            var submissionId = (await _dataHelper.Submissions().GetAnyAsync()).Id;
+
+            // Act
+            var submissionResult = _connector.SubmissionResult(submissionId, null, 200, 5000);
+            var deserialized = JObject.Parse(submissionResult);
+
+            // Assert
+            submissionResult.Should().NotBeNull();
+            deserialized.ContainsKey("submission_id").Should().BeTrue();
+            deserialized.ContainsKey("results").Should().BeTrue();
+        }
+    }
+}
---
+++ Indico.AutomationAnywhere.Connector.IntegrationTests/Utils/DataHelpers/Submissions/SubmissionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using IndicoV2.Submissions;
+using IndicoV2.Submissions.Models;
+
+namespace Indico.AutomationAnywhere.Connector.IntegrationTests.Utils.DataHelpers.Submissions
+{
+    public class SubmissionHelper
+    {
+        private readonly ISubmissionsClient _submissions;
+
+        public SubmissionHelper(ISubmissionsClient submissions) => _submissions = submissions;
+
+        public async Task<ISubmission> GetAnyAsync() => (await _submissions.ListAsync(null, null, null, 1)).Single();
+
+    }
+}
