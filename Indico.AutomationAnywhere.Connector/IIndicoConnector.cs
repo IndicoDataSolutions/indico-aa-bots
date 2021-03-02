70c8b2d3c7c953bcf728620d4a26cd1318810aa4
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-02 08:28:05 +0100

    INT-50 Added SubmissionResult method to the connector.

--- Indico.AutomationAnywhere.Connector/IIndicoConnector.cs
+++ Indico.AutomationAnywhere.Connector/IIndicoConnector.cs
@@ -22,5 +22,15 @@ namespace Indico.AutomationAnywhere.Connector
         /// <param name="workflowId"><c><see cref="int">Id</see></c> of the workflow to submit data to.</param>
         /// <returns>New submissions <c><see cref="int">ids</see></c>.</returns>
         int[] WorkflowSubmission(string[] filepaths, string[] uris, int workflowId);
+
+        /// <summary>
+        /// Method gets result of the submission.
+        /// </summary>
+        /// <param name="submissionId">Id of the submission to get.</param>
+        /// <param name="checkStatus">Optional parameter that causes wait until submissions is in certain state.</param>
+        /// <param name="checkIntervalMilliseconds">Interval in miliseconds of checking submission status.</param>
+        /// <param name="timeoutMilliseconds">Time in miliseconds before timeout of the function.</param>
+        /// <returns>The result of submission in form of a JSON.</returns>
+        string SubmissionResult(int submissionId, string checkStatus, int checkIntervalMilliseconds, int timeoutMilliseconds);
     }
 }
--- Indico.AutomationAnywhere.Connector/IndicoConnector.cs
+++ Indico.AutomationAnywhere.Connector/IndicoConnector.cs
@@ -2,13 +2,16 @@
 using System.Linq;
 using System.Threading.Tasks;
 using IndicoV2;
+using IndicoV2.Extensions.SubmissionResult;
 using IndicoV2.Submissions;
+using IndicoV2.Submissions.Models;
 
 namespace Indico.AutomationAnywhere.Connector
 {
     public class IndicoConnector : IIndicoConnector
     {
         private ISubmissionsClient _submissionsClient;
+        private ISubmissionResultAwaiter _submissionResultAwaiter;
 
         /// <summary>
         /// Constructor for AutomationAnywhere
@@ -18,12 +21,17 @@ namespace Indico.AutomationAnywhere.Connector
 
         }
 
-        public IndicoConnector(ISubmissionsClient submissionsClient) => _submissionsClient = submissionsClient;
+        public IndicoConnector(ISubmissionsClient submissionsClient, ISubmissionResultAwaiter submissionResultAwaiter)
+        {
+            _submissionsClient = submissionsClient;
+            _submissionResultAwaiter = submissionResultAwaiter;
+        }
 
         public void Init(string token, string uri)
         {
             var client = new IndicoV2.IndicoClient(token, new Uri(uri));
             _submissionsClient = client.Submissions();
+            _submissionResultAwaiter = client.GetSubmissionResultAwaiter();
         }
 
         public int[] WorkflowSubmission(string[] filepaths, string[] uris, int workflowId)
@@ -52,6 +60,26 @@ namespace Indico.AutomationAnywhere.Connector
             }
 
             return Task.Run(async () => await _submissionsClient.CreateAsync(workflowId, uris.Select(u => new Uri(u)))).GetAwaiter().GetResult().ToArray();
+        }
+
+        public string SubmissionResult(int submissionId, string checkStatus, int checkIntervalMilliseconds, int timeoutMilliseconds)
+        {
+            var awaitStatus = string.IsNullOrWhiteSpace(checkStatus)
+                ? (SubmissionStatus?)null
+                : (SubmissionStatus)Enum.Parse(typeof(SubmissionStatus), checkStatus);
+
+            var checkInterval = TimeSpan.FromMilliseconds(checkIntervalMilliseconds);
+            var timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+            var getResult = awaitStatus == null
+                ? Task.Run(async () => await _submissionResultAwaiter.WaitReady(submissionId, checkInterval, timeout))
+                : Task.Run(async () => await _submissionResultAwaiter.WaitReady(submissionId, awaitStatus.Value, checkInterval, timeout));
+
+            var result = getResult
+                .GetAwaiter()
+                .GetResult();
+
+            return result.ToString();
         }
     }
 }
