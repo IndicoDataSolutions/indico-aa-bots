4934c80e9565deedadd8efe4c64b48dd2c046ac0
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-04 16:41:54 +0100

    INT-52 Added extra integration test when no changes ad no rejected.

--- Indico.AutomationAnywhere.Connector.IntegrationTests/IndicoConnectorTests/SubmitReviewTests.cs
+++ Indico.AutomationAnywhere.Connector.IntegrationTests/IndicoConnectorTests/SubmitReviewTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Indico.Exception;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
@@ -11,7 +13,7 @@ namespace Indico.AutomationAnywhere.Connector.IntegrationTests.IndicoConnectorTe
         public async Task SubmitReview_ShouldReturnResult()
         {
             // Arrange
-            var submissionResult = await _dataHelper.Submissions().GetResult(10060);//GetNewResult();
+            var submissionResult = await _dataHelper.Submissions().GetNewResult();
             var submissionId = submissionResult.Value<int>("submission_id");
             var changes = submissionResult.SelectToken("results.document.results").ToString();
 
@@ -22,6 +24,20 @@ namespace Indico.AutomationAnywhere.Connector.IntegrationTests.IndicoConnectorTe
             // Assert
             deserializedSubmitReviewResult.Value<string>("submission_status").Should().Be("pending_review");
             deserializedSubmitReviewResult.Value<bool>("success").Should().Be(true);
+        }
+
+        [Test]
+        public async Task SubmitReview_ShouldThrowIfNoChangesAndNotRejected()
+        {
+            // Arrange
+            var submissionResult = await _dataHelper.Submissions().GetNewResult();
+            var submissionId = submissionResult.Value<int>("submission_id");
+
+            // Act
+            Action act = () => _connector.SubmitReview(submissionId, null, false);
+
+            // Assert
+            act.Should().Throw<RuntimeException>().WithMessage("Must provide Changes or Reject=true");
         }
     }
 }
