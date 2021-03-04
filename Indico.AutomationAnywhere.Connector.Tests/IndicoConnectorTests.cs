4bcd1dccc7ca44712d4675d815775c7ffb6f9904
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-04 13:23:10 +0100

    INT-52 Added Submit review unit tests.

--- Indico.AutomationAnywhere.Connector.Tests/IndicoConnectorTests.cs
+++ Indico.AutomationAnywhere.Connector.Tests/IndicoConnectorTests.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using FluentAssertions;
+using IndicoV2.Extensions.Jobs;
+using IndicoV2.Reviews;
 using IndicoV2.Submissions;
 using IndicoV2.Submissions.Models;
 using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using SubmissionFilterV2 = IndicoV2.Submissions.Models.SubmissionFilter;
 
@@ -15,12 +19,16 @@ namespace Indico.AutomationAnywhere.Connector.Tests
     {
         private IIndicoConnector _connector;
         private Mock<ISubmissionsClient> _submissionsClientMock;
+        private Mock<IReviewsClient> _reviewsClientMock;
+        private Mock<IJobAwaiter> _jobAwaiterMock;
 
         [SetUp]
         public void Setup()
         {
             _submissionsClientMock = new Mock<ISubmissionsClient>();
-            _connector = new IndicoConnector(_submissionsClientMock.Object);
+            _reviewsClientMock = new Mock<IReviewsClient>();
+            _jobAwaiterMock = new Mock<IJobAwaiter>();
+            _connector = new IndicoConnector(_submissionsClientMock.Object, _reviewsClientMock.Object, _jobAwaiterMock.Object);
         }
 
         [Test]
@@ -133,6 +141,38 @@ namespace Indico.AutomationAnywhere.Connector.Tests
 
             //Assert
             act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        public void SubmitReview_ShouldCallReviewsClient(bool rejected, bool? forceComplete)
+        {
+            // Arrange
+            var submissionId = 1;
+            var changes = JObject.FromObject(new { name = "test", id = 12, boolCol = true });
+            var changesJson = changes.ToString();
+            const int idValue = 2;
+
+            _jobAwaiterMock.Setup(cli =>
+                    cli.WaitReadyAsync(It.IsAny<string>(), It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(JToken.FromObject(new { id = idValue }));
+
+            // Act
+            var result = forceComplete.HasValue
+                ? _connector.SubmitReview(submissionId, changesJson, rejected, forceComplete.Value)
+                : _connector.SubmitReview(submissionId, changesJson, rejected);
+
+            var deserializedResult = JObject.Parse(result);
+
+            // Assert
+            _reviewsClientMock.Verify(cli => cli.SubmitReviewAsync(
+                submissionId,
+                It.Is<JObject>(actualChanges => JToken.DeepEquals(actualChanges, changes)),
+                rejected,
+                forceComplete,
+                It.IsAny<CancellationToken>()),
+                Times.Once);
+            result.Should().NotBeNull();
+            deserializedResult.Value<int>("id").Should().Be(idValue);
         }
     }
 }
